2de1c6a04c40a9210ef88ee94fb77c29783a413e
SamuelRufino <samuelrufino0103@gmail.com>
2024-05-15 19:43:54 -0300

    model pet

--- adotai/adotaiapp/models.py
+++ adotai/adotaiapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class pet(models.Model):
+    Nome = models.CharField(max_length=100)
+    Idade = models.IntegerField(max_length=100)
+    Caracteristicas = models.CharField(max_length=255)
