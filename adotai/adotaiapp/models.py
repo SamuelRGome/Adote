6173eeeae00c7056bfbc61bbe30d4608cb131226
SamuelRufino <samuelrufino0103@gmail.com>
2024-05-15 19:46:30 -0300

    adição do model no admin

--- adotai/adotaiapp/models.py
+++ adotai/adotaiapp/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class pet(models.Model):
     Nome = models.CharField(max_length=100)
-    Idade = models.IntegerField(max_length=100)
+    Idade = models.IntegerField()
     Caracteristicas = models.CharField(max_length=255)
