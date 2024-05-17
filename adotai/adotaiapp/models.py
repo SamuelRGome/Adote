8442e6c23cc909c75f9314d595b8c0e36172245d
SamuelRufino <samuelrufino0103@gmail.com>
2024-05-17 04:51:23 -0300

    mudança de diretório da imagem do model

--- adotai/adotaiapp/models.py
+++ adotai/adotaiapp/models.py
@@ -2,7 +2,9 @@ from django.db import models
 
 # Create your models here.
 class pet(models.Model):
-    Img = models.ImageField(null= True, blank= True, upload_to="Img_pet/")
+    Img = models.ImageField(null= True, blank= True, upload_to="static/imagens")
     Nome = models.CharField(max_length=100)
     Idade = models.IntegerField()
     Caracteristicas = models.CharField(max_length=255)
+    def __str__(self):
+        return self.Nome
--- adotai/adotaiapp/views.py
+++ adotai/adotaiapp/views.py
@@ -2,7 +2,5 @@ from django.shortcuts import render
 from . import models
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
-
-def pets(request):
-    return render(request,'models.pet')
+    anim = models.pet.objects.all()
+    return render(request,'index.html', {'anim': anim})
