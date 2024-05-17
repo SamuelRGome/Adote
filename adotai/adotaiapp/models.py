6103169dc16b712ec8518f89e7aac1560a88240e
SamuelRufino <samuelrufino0103@gmail.com>
2024-05-17 07:16:45 -0300

    imagens no html

--- adotai/adotaiapp/models.py
+++ adotai/adotaiapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class pet(models.Model):
-    Img = models.ImageField(null= True, blank= True, upload_to="static/imagens")
+    Img = models.ImageField(upload_to="djangouploads/files/covers")
     Nome = models.CharField(max_length=100)
     Idade = models.IntegerField()
     Caracteristicas = models.CharField(max_length=255)
