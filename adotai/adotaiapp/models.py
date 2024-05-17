4136397c1443f3bac9bbe19837d14844177f0da3
SamuelRufino <samuelrufino0103@gmail.com>
2024-05-17 03:17:19 -0300

    migrations e img

--- adotai/adotaiapp/models.py
+++ adotai/adotaiapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class pet(models.Model):
-    Img = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
+    Img = models.ImageField(null= True, blank= True, upload_to="Img_pet/")
     Nome = models.CharField(max_length=100)
     Idade = models.IntegerField()
     Caracteristicas = models.CharField(max_length=255)
--- adotai/adotaiapp/views.py
+++ adotai/adotaiapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from . import models
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def pets(request):
+    return render(request,'models.pet')
