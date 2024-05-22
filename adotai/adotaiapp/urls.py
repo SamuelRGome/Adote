3899990f2059760896306aa7191969dc156e9384
SamuelRufino <samuelrufino0103@gmail.com>
2024-05-22 12:07:41 -0300

    links

--- adotai/adotaiapp/urls.py
+++ adotai/adotaiapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.peludos, name='peludos')
+    path('', views.peludos, name='peludos'),
+    path('', views.out, name='out')
 ]
--- adotai/adotaiapp/views.py
+++ adotai/adotaiapp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from . import models
+from django.http import HttpResponseRedirect
 # Create your views here.
 def peludos(request):
     anim = models.pet.objects.all()
     return render(request,'peludos.html', {'anim': anim})
+
+def out(request):
+    return HttpResponseRedirect("Instagram.com", {'out': out})
